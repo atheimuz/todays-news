c5d3a98291f7df0399eab7e856cd0ad6b87198b6
atheimuz <atheimuz@naver.com>
2024-12-21 22:34:53 +0900

    feat: 트렌드 리스트 응답에서 뉴스 정보 제거

--- src/app/api/trends/route.ts
+++ src/app/api/trends/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { parseStringPromise } from "xml2js";
-import { fetchMetaImage } from "@/utils/fetch-meta";
 
 function percentToNumber(input: string) {
     const number = parseFloat(input.replace(/,/g, "").replace("+", ""));
@@ -26,33 +25,13 @@ export async function GET() {
         const jsonData = await parseStringPromise(xmlData, { explicitArray: false });
         const items = jsonData.rss.channel.item;
 
-        const result = await Promise.all(
-            items.map(async (item) => {
-                let news = [];
-                if (item?.["ht:news_item"].length > 0) {
-                    news = await Promise.all(
-                        item?.["ht:news_item"]?.map(async (newsItem) => {
-                            const thumbnail = await fetchMetaImage(newsItem["ht:news_item_url"]);
-                            return {
-                                link: newsItem["ht:news_item_url"],
-                                title: newsItem["ht:news_item_title"],
-                                thumbnail: replaceImgUrl(
-                                    newsItem["ht:news_item_picture"] || thumbnail
-                                ),
-                                company: newsItem["ht:news_item_source"]
-                            };
-                        })
-                    );
-                }
-
-                return {
-                    name: item.title,
-                    thumbnail: replaceImgUrl(item["ht:picture"]),
-                    score: percentToNumber(item["ht:approx_traffic"]),
-                    news
-                };
-            })
-        );
+        const result = items.map((item) => {
+            return {
+                name: item.title,
+                thumbnail: replaceImgUrl(item["ht:picture"]),
+                score: percentToNumber(item["ht:approx_traffic"])
+            };
+        });
 
         return NextResponse.json({ status: 200, data: result });
     } catch (error) {
