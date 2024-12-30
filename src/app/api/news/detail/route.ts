3df5194de250beda0b7aaa5e21af4b7239ac3351
atheimuz <atheimuz@naver.com>
2024-12-30 22:40:55 +0900

    feat: 뉴스 상세 내용 가져오는 api 추가

---
+++ src/app/api/news/detail/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { fetchMetaText } from "@/utils/fetch-meta";
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const link = searchParams.get("link");
+
+    if (!link) {
+        return NextResponse.json({ status: 200, data: null });
+    }
+
+    try {
+        const response = await fetch(link);
+        const htmlData = await response.text();
+        const content = await fetchMetaText(htmlData);
+
+        return NextResponse.json({ status: 200, data: content });
+    } catch (error) {
+        return NextResponse.json(
+            {
+                success: false,
+                message: `Error fetching news. detail: ${
+                    error instanceof Error ? error.message : "unknown"
+                }`
+            },
+            { status: 500 }
+        );
+    }
+}
