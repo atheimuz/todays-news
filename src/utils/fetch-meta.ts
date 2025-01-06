33dd9f16b38b869da2af30fbb098d237d22d1e74
atheimuz <atheimuz@naver.com>
2025-01-06 20:07:52 +0900

    feat: 메타 정보 가져오는 정규식 변경

--- src/utils/fetch-meta.ts
+++ src/utils/fetch-meta.ts
@@ -62,18 +62,15 @@ const cleanImageSources = (document: Document): void => {
 
 // 기사 이미지를 가져오는 함수
 export const fetchMetaImage = async (htmlData: string) => {
-    try {
-        const ogImageMatch = htmlData.match(/<meta property="og:image" content="([^"]+)"/);
-        const twitterImageMatch = htmlData.match(/<meta name="twitter:image" content="([^"]+)"/);
+    const ogImageMatch = htmlData.match(/<meta[^>]*property="og:image"[^>]*content="([^"]+)"/);
+    const twitterImageMatch = htmlData.match(
+        /<meta[^>]*name="twitter:image"[^>]*content="([^"]+)"/
+    );
 
-        if (ogImageMatch) return ogImageMatch[1];
-        if (twitterImageMatch) return twitterImageMatch[1];
+    if (ogImageMatch) return ogImageMatch[1];
+    if (twitterImageMatch) return twitterImageMatch[1];
 
-        return null;
-    } catch (error) {
-        console.error("Error fetching the meta image:", error);
-        return null;
-    }
+    return null;
 };
 
 // 기사 텍스트를 가져오는 함수
